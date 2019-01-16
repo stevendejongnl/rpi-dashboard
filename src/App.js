8bdc4f67809f55075e2651402e7bfe20c8ad7c0f
Steven de Jong <steven@steven-dejong.nl>
2019-01-16 08:40:21 +0100

    Init dashboard

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Spotify from './Components/Spotify'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+          <Spotify />
+      </>
+    )
+  }
+}
+
+export default App
