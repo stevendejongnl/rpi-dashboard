ebb5f212454995350af0fd96e80a39af6beac38a
Steven de Jong <steven@steven-dejong.nl>
2019-01-16 11:00:05 +0100

    ..

--- system/tools/spotify/index.php
+++ system/tools/spotify/index.php
@@ -1,13 +1,19 @@
 <?
 
 // Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
+//if (isset($_SERVER['HTTP_ORIGIN'])) {
     // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
     // you want to allow, and if so:
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
+//    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+//    header('Access-Control-Allow-Credentials: true');
+//    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+//}
+
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
