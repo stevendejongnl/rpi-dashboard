065bf965e10ab687f958b9a832942bfe3204987b
Steven de Jong <steven@steven-dejong.nl>
2019-01-16 10:57:59 +0100

    Allow Origin http origin

--- system/tools/spotify/index.php
+++ system/tools/spotify/index.php
@@ -1,6 +1,13 @@
 <?
 
-header("Access-Control-Allow-Origin: *");
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
